5e8cd24a4254cf10e3b8110f5744d757cbc01777
prestonraab <prestonraab@gmail.com>
2023-05-26 01:08:17 -0600

    Add error

--- discord_bot2.py
+++ discord_bot2.py
@@ -207,7 +207,9 @@ class MyClient(discord.Client):
                 if os.system(f"git checkout {message_args.branch}") != 0:
                     await message.channel.send(f'Error checking out {message_args.branch} branch.')
                     return
-                os.system("git clean -fxd")
+                if os.system("git clean -fxd") != 0:
+                    await message.channel.send('Error cleaning git.')
+                    return
                 if os.system("git pull") != 0:
                     await message.channel.send('Error pulling from git.')
                     return
