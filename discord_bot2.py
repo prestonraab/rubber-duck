ebae543c0cbbe0a8354bcb1cba9d8e44c7e1d36c
prestonraab <prestonraab@gmail.com>
2023-06-05 14:52:15 -0600

    Try format again

--- discord_bot2.py
+++ discord_bot2.py
@@ -212,10 +212,10 @@ class MyClient(discord.Client):
         # Get output of command and send to channel
         errors = process.stderr.decode('utf-8')
         if errors:
-            await self.say_in_channel(channel, f'Errors: ```bash\n{errors}```')
+            await self.say_in_channel(channel, errors, prefix=f'Errors: ```bash\n', suffix='```')
         output = str(process.stdout.decode('utf-8'))
         if output:
-            await self.say_in_channel(channel, f'Output: ```bash\n{output}```')
+            await self.say_in_channel(channel, output, prefix=f'Output: ```bash\n', suffix='```')
         if len(output) > 2000:
             await channel.send(f'Done.')
         return
