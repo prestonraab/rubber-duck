cd413617e63d96f3184c2f9170e9e54a798f8098
prestonraab <prestonraab@gmail.com>
2023-06-09 16:01:09 -0600

    Back to bot2, oops

--- restart.sh
+++ restart.sh
@@ -1,2 +1,2 @@
 kill "$(ps -ae | grep python | awk '{$1=$1};1' | cut -d ' ' -f 1)"
-nohup poetry run python discord_bot3.py >> /tmp/duck.log &
+nohup poetry run python discord_bot2.py >> /tmp/duck.log &
