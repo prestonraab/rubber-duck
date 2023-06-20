9ab56725983f935e0a3536fec6b1857265f11e7f
prestonraab <prestonraab@gmail.com>
2023-06-20 00:11:41 -0600

    Specific prompt

--- discord_bot3.py
+++ discord_bot3.py
@@ -137,6 +137,7 @@ class DuckResponseFlow:
             "I'm doing scheduling",
             "Help with simplify",
             "Help on moose",
+            "Can you help me with invert-careful?"
         ]
         example_topics = [
             "functions",
@@ -150,7 +151,7 @@ class DuckResponseFlow:
         Examples of last messages that indicate a conversation is over: {'"'}{'", "'.join(done_examples)}{'"'}.
         If answering a question below requires assignment-specific context, retrieve that assignment.
         Examples of assignment names: {'"'}{'", "'.join(example_assignments)}{'"'}.
-        If answering a question below requires problem-specific context, identify the assignment the problem belongs to, and retrieve that assignment.
+        If answering a question below requires problem-specific context, retrieve the assignment that the problem belongs to.
         Examples of questions that require problem-specific context: {'"'}{'", "'.join(example_problems)}{'"'}.
         If a question below relates to a specific Python topic, retrieve context from the appropriate guide entry.
         Examples of topics: {'"'}{'", "'.join(example_topics)}{'"'}.'''
